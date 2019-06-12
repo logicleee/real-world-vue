c6504c85201609be3029b1f3191f11c1bb5766ba
lee <logicleeee+github@gmail.com>
2019-06-11 20:38:49 -0700

    completed lesson 4

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <!-- Updated template to named routes -->
+      <router-link :to="{ name: 'event-list'}">List</router-link> |
+      <router-link :to="{ name: 'event-show'}">Show Event #1</router-link> |
+      <router-link :to="{ name: 'event-create'}">Create</router-link> |
     </div>
     <router-view />
   </div>
