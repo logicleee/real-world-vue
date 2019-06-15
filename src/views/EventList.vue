6c7d1630d7d77b21638602715365ff227e828d5d
lee <logicleeee+github@gmail.com>
2019-06-14 20:02:12 -0700

    completed Vuex lesson 6

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,40 +1,49 @@
 <template>
   <div>
-    <h1>Events Listing</h1>
-    <EventCard v-for="event in events" :key="event.id" :event="event"/>
+    <h1>Events for {{ user.user.name }}</h1>
+    <EventCard v-for="event in event.events" :key="event.id" :event="event" />
     <template v-if="page != 1">
-      <router-link :to="{ name: 'event-list', query: { page: page - 1 } }" rel="prev">Prev Page</router-link> |
+      <router-link
+        :to="{ name: 'event-list', query: { page: page - 1 } }"
+        rel="prev"
+        >Prev Page</router-link
+      >
+      |
     </template>
     <template v-if="hasNextPage">
-      <router-link :to="{ name: 'event-list', query: { page: page + 1 } }" rel="next">Next Page</router-link>
+      <router-link
+        :to="{ name: 'event-list', query: { page: page + 1 } }"
+        rel="next"
+        >Next Page</router-link
+      >
     </template>
   </div>
 </template>
 
 <script>
-import EventCard from '@/components/EventCard.vue'
-import { mapState, mapGetters } from 'vuex'
-
+import EventCard from '@/components/EventCard.vue';
+import { mapState, mapActions } from 'vuex';
 
 export default {
   components: {
     EventCard
   },
   created() {
-    this.perPage = 3
-    this.$store.dispatch('fetchEvents', {
+    this.perPage = 3;
+    this.fetchEvents({
       perPage: this.perPage,
       page: this.page
-    })
+    });
   },
   computed: {
     page() {
-      return parseInt(this.$route.query.page) || 1
+      return parseInt(this.$route.query.page) || 1;
     },
     hasNextPage() {
-      return this.eventsTotalCount > this.page * this.perPage
+      return this.event.eventsTotalCount > this.page * this.perPage;
     },
-    ...mapState(['events', 'eventsTotalCount'])
-  }
-}
+    ...mapState(['event', 'user'])
+  },
+  methods: mapActions('event', ['fetchEvents'])
+};
 </script>
