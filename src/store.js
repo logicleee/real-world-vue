021e6452599f9bb78629f1e27c5798bfe5ffa773
lee <logicleeee+github@gmail.com>
2019-06-14 18:29:21 -0700

    completed Vuex lesson 5

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import EventService from '@/services/EventService';
+import Vue from 'vue'
+import Vuex from 'vuex'
+import EventService from '@/services/EventService'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
@@ -16,23 +16,61 @@ export default new Vuex.Store({
       'food',
       'community'
     ],
-    events: []
+    events: [],
+    event: {},
+    eventsTotalCount: 0
   },
   mutations: {
     ADD_EVENT(state, event) {
-      state.events.push(event);
+      state.events.push(event)
+    },
+    SET_EVENTS(state, events) {
+      state.events = events
+    },
+    SET_EVENTS_TOTAL_COUNT(state, count) {
+      state.eventsTotalCount = count
+    },
+    SET_EVENT(state, event) {
+      state.event = event
     }
   },
   actions: {
     createEvent({ commit }, event) {
       return EventService.postEvent(event).then(() => {
-        commit('ADD_EVENT', event);
-      });
+        commit('ADD_EVENT', event)
+      })
+    },
+    fetchEvents({ commit }, { perPage, page }) {
+      EventService.getEvents(perPage, page)
+        .then(response => {
+          commit(
+            'SET_EVENTS_TOTAL_COUNT',
+            parseInt(response.headers['x-total-count'])
+          )
+          commit('SET_EVENTS', response.data)
+        })
+        .catch(error => {
+          console.log('There was an error:', error.response)
+        })
+    },
+    fetchEvent({ commit, getters }, id) {
+      const event = getters.getEventById(id)
+
+      if (event) {
+        commit('SET_EVENT', event)
+      } else {
+        EventService.getEvent(id)
+          .then(response => {
+            commit('SET_EVENT', response.data)
+          })
+          .catch(error => {
+            console.log('there was an error:', error.response)
+          })
+      }
     }
   },
   getters: {
-    categoriesLength: (state) => state.categories.length,
-    getEventById: (state) => (id) =>
-      state.events.find((event) => event.id === id)
+    categoriesLength: state => state.categories.length,
+    getEventById: state => id => state.events.find(event => event.id === id)
   }
-});
+})
